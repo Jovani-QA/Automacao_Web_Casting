db29fabff1bc1e798c547f9dca9470300f011395
Jovani Miguel <repository.casting@gmail.com>
2022-09-29 22:29:02 -0300

    Alterando comentários

--- src/test/java/config/config_Web.java
+++ src/test/java/config/config_Web.java
@@ -10,7 +10,7 @@ public class config_Web {
     public WebDriver WebDriver()
     {
         WebDriver driver = new ChromeDriver();
-        driver.get("http://169.62.128.221/Casting/CASTING_ADM_HOMO");
+        driver.get("http://169.62.128.221/Casting/CASTING_ADM_HOM");
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
         return driver;
